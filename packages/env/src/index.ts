d491b21261fbf07c0af4947716d4aef89a745b97
Brendan Dash <me@aiwan.run>
2025-07-20 17:45:26 +0800

    chore: deps catalog

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,20 +1,24 @@
+/* eslint-disable n/prefer-global/process */
 import path from 'node:path'
 import { createEnv } from '@t3-oss/env-core'
 import { z } from 'zod'
+
+const isDev = process.env.NODE_ENV === 'development'
 
 export const env = createEnv({
   server: {
     DB_FILE_NAME: z.string(),
   },
-
   runtimeEnv: (() => {
-    const __dirname = path.dirname(new URL(import.meta.url).pathname)
+    if (isDev) {
+      const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
-    const envPath = path.join(__dirname, '../../../.env')
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('dotenv').config({ path: envPath })
+      const envPath = path.join(__dirname, '../../../.env')
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require('dotenv').config({ path: envPath })
+    }
 
-    // eslint-disable-next-line n/prefer-global/process
     return process?.env ?? {}
   })(),
+  skipValidation: !isDev,
 })
